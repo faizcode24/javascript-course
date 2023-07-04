a3125b3ff6ef4b21bfeafdc3c0571a927fd36b19
faizcode24 <mdfaiz589@gmail.com>
2023-07-04 16:17:25 +0530

    new commit

---
+++ 17.operators/custom.js
@@ -0,0 +1,28 @@
+// logical operator
+// it gives the ability to check more than one codition 
+// && AND (BOTH MUST BE TRUE)
+// || OR (EITHER CONDTION CANBE TRUE )
+
+let temp = 50;
+
+if(temp > 0 && temp < 30){
+    console.log("the temp is good");
+
+}
+else{
+    console.log("it is not")
+}
+
+// above is the logical operator
+
+
+//now lets see the not logical operator
+
+// typically used to reverse a condition boolean value
+// true -> false false -> true
+
+
+let ttemp = 34;
+if(!(temp > 34)){
+    console.log("new temp");
+}
---
+++ 18.Loops/custom.js
@@ -0,0 +1,32 @@
+// // while loop
+//  repeat some code while some codition is true
+//     potentially Infinity
+
+
+// let userNmae = " ";
+
+// while(userNmae == " "){
+//     userNmae = window.prompt("Entre your name");
+// }
+
+// console.log("Hello", userNmae);
+
+//do while loop == do somethingm then check the condition,
+            //      repeat if conditon is true
+
+
+// let userName ;
+
+// do{
+//     userName= window.prompt("Entre your name");
+// }while(userName== " ")
+// console.log("Hello", userName);
+
+
+//  for loop = repeat some code a certain amount of time
+
+for(let i = 0 ; i < 10; i +=3){
+    console.log(i);
+}
+
+console.log("Happy new year");
